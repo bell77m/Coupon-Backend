fc53672f3330661e9ab1c6f1521e31d42f2d28d6
bell77m <bell77m@gmail.com>
2025-06-06 04:44:08 +0700

    add table

---
+++ sql/coupon.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `users` (
+  `id` integer PRIMARY KEY,
+  `user_nisit` varchar(255),
+  `password` varchar(255),
+  `coupon_meal_used` integer,
+  `coupon_sweet_used` integer,
+  `created_at` timestamp
+);
+
+CREATE TABLE `stores` (
+  `id` integer PRIMARY KEY,
+  `store_type` varchar(255),
+  `user_store` varchar(255),
+  `password` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `orders` (
+  `id` integer PRIMARY KEY,
+  `user_id` integer NOT NULL,
+  `store_id` integer NOT NULL,
+  `created_at` timestamp
+);
+
+ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `orders` ADD FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`);
